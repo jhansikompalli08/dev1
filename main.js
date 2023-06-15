d0e60c4b8e9a7ee405f79e6340801fca11f87bb4
jhansi_kompalli <jhansi08kompalli@gmail.com>
2023-06-15 13:21:23 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,14 @@
+//console.dir(document);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.getElementById('header-title'));
+//var headerTitle=document.getElementById('header-title');
+//headerTitle.textContent='hello';
+ 
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[3]);
+items[2].style.backgroundColor='green';
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
