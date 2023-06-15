3051e568b97a1533d810cb3cdb44972cc0fbb64d
jhansi_kompalli <jhansi08kompalli@gmail.com>
2023-06-15 14:36:24 +0530

    3commit

--- main.js
+++ main.js
@@ -5,10 +5,13 @@
 //var headerTitle=document.getElementById('header-title');
 //headerTitle.textContent='hello';
  
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[3]);
-items[2].style.backgroundColor='green';
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+//var items=document.getElementsByClassName('list-group-item');
+//console.log(items);
+//console.log(items[3]);
+//items[2].style.backgroundColor='green';
+//for(var i=0;i<items.length;i++){
+    //items[i].style.fontWeight='bold';
+//}
+ var li=document.getElementsByTagName('li');
+ console.log(li);
+ console.log(li[1]);
